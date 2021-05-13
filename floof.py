cf1ab8993cd9b2c6014fed30f888ad6e85a4c7ad
Julian Imhof <julianimhof00@gmail.com>
2021-05-13 04:00:36 +0200

    fixing everything

--- floof.py
+++ floof.py
@@ -1,14 +1,12 @@
 # I know what you guys are about to say... "YOU SAID NO UPDATES", you loudly shriek! Guess what?! I got bored. Here you go. It sucks really bad and can be optimized a lot. Just look at it wow cool plz lol.
 # Someone commit to my shitty sniper plz
 import requests
-import aiohttp
 import time
-import os
-import datetime
-import asyncio
+import http.client, json, threading, ssl
+from dateutil.parser import isoparse
+
 f = open("bearer.txt", "r")
 bearerlist = f.readlines()
-#os.system('cls')
 thetitle = f"""███████╗██╗░░░░░░█████╗░░█████╗░███████╗  ░██████╗███╗░░██╗██╗██████╗░███████╗██████╗░
 ██╔════╝██║░░░░░██╔══██╗██╔══██╗██╔════╝  ██╔════╝████╗░██║██║██╔══██╗██╔════╝██╔══██╗
 █████╗░░██║░░░░░██║░░██║██║░░██║█████╗░░  ╚█████╗░██╔██╗██║██║██████╔╝█████╗░░██████╔╝
@@ -20,25 +18,47 @@ othertext = f"""(╯°o°)╯ Multi-Bearer Edition!"""
 print(othertext)
 print('Made By a Hippo')
 name = input("Name: ")
-#bearer = input("Bearer: ")
 delay = int(input("Delay: "))
-droptime = time.mktime(
-	datetime.datetime.fromisoformat(
-		requests.get(f'https://api.nathan.cx/check/{name}').json()['drop_time'][:-1]).replace(
-		tzinfo=datetime.timezone.utc).astimezone().timetuple())
-async def main():
-    headers={"Content-type": "application/json", "Authorization": "Bearer " + line}
-    json={"profileName": name}
-    async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.post("https://api.minecraftservices.com/minecraft/profile", json=json) as r:
-            #json_body = await r.json()
-            print('Request sent @ ' + str(time.time()) + ' with the code ' + str(r.status))
+try:
+    jj = requests.get(f'https://api.nathan.cx/check/{name}').json()
+except Exception:
+    print("Unexpected Error")
+    exit()
+
+if not jj.get('drop_time'):
+    if jj.get('status') and jj['status'] == 'taken':
+        print("Error, name taken")
+    print("Unexpected Error")
+    exit()
+
+droptime = isoparse(jj['drop_time']).timestamp()
+e = threading.Event()
+
+def runRequest(line):
+    headers = {"Accept": "application/json", "Authorization": "Bearer " + line}
+    jsn     = {"profileName": name}
+    jsn     = json.dumps(jsn)
+    conn    = http.client.HTTPSConnection("api.minecraftservices.com")
+    e.wait()
+    conn.request("POST", "/minecraft/profile", jsn, headers)
+    response = conn.getresponse()
+    print("Got answer at", time.time())
+    print(response.status, response.reason)
+    print(response.read().decode())
+
 if droptime + - time.time() > 60:
     print('Sniping ' + name + ' in ' + str(round((droptime + - time.time()) / 60 )) + ' minutes!')
 if droptime + - time.time() < 60:
     print('Sniping ' + name + ' in ' + str(round(droptime + - time.time())) + ' seconds!')
-time.sleep(droptime + - time.time() - (delay / 1000))
+
+threads = []
 for line in bearerlist:
-	loop = asyncio.get_event_loop()
-	coroutines = [main() for _ in range(6)]
-	results = loop.run_until_complete(asyncio.gather(*coroutines))
+    line = line.strip()
+    for i in range(6):
+        threads += [threading.Thread(target=runRequest, args=(line, ))]
+for t in threads:
+    t.start()
+
+time.sleep(droptime + - time.time() - (delay / 1000))
+print("Starting requests at", time.time())
+e.set()
